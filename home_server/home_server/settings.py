141a46d2eccb1cea79d5c7e051ca3b12660a0880
w.twarkiewic <w.twarkiewic@samsung.com>
2020-05-01 18:11:14 +0200

    prepared for deployment

--- home_server/home_server/settings.py
+++ home_server/home_server/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = 'static_files'
 #Below is working for Windows
 STATICFILES_DIRS = (os.path.join('static'),)
